5e448aafdc4fb96897c5f08c64239ce1134b6e49
Gyu-Yeon <idsopy@nate.com>
2022-12-06 03:28:08 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import searchImages from "./api";
+
 function App() {
+  const handleClick = () => {
+    searchImages();
+  };
   return (
     <div className="App">
       <h1>App</h1>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
